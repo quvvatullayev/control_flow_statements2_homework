390609b3c6e29a37e971acb5993f0594504286fe
tajriba <quvvatullayev@gmail.com>
2022-06-22 08:19:05 +0300

    if05.py

--- if05.py
+++ if05.py
@@ -6,4 +6,33 @@ def main(n):
     Returns:
         int: return answer.
     """
-    return
+
+    answer = 0
+
+    n1 = n % 10
+    n //= 10
+
+    n2 = n % 10
+    n //= 10
+
+    n3 = n % 10
+    n //= 10
+
+    n4 = n % 10
+    n //= 10
+
+    n5 = n % 10
+    n //= 10
+
+    if n1 > n2 and n1 > n3 and n1 > n4 and n1 > n5:
+        answer += n1
+
+    elif n2 > n1 and n2 > n3 and n2 > n4 and n2 > n5:
+        answer += n2
+
+    elif n3 > n1 and n3 > n2 and n3 > n4 and n3 > n5:
+        answer += n3
+
+    return answer
+
+print(main(56843))
