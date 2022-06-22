5b27530ef0545866ddc0e07188dbb73763355911
tajriba <quvvatullayev@gmail.com>
2022-06-22 08:36:07 +0300

    if06.py

--- if06.py
+++ if06.py
@@ -25,19 +25,19 @@ def main(n):
     n //= 10
 
     if n1 > n2 and n1 > n3 and n1 > n4 and n1 > n5:
-        answer += 0
-
-    elif n2 > n1 and n2 > n3 and n2 > n4 and n2 > n5:
         answer += 1
 
-    elif n3 > n1 and n3 > n2 and n3 > n4 and n3 > n5:
+    elif n2 > n1 and n2 > n3 and n2 > n4 and n2 > n5:
         answer += 2
 
-    elif n4 > n2 and n4 > n3 and n4 > n5 and n4 > n1:
+    elif n3 > n1 and n3 > n2 and n3 > n4 and n3 > n5:
         answer += 3
 
-    elif n5 > n2 and n5 > n3 and n5 > n4 and n5 > n1:
+    elif n4 > n2 and n4 > n3 and n4 > n5 and n5 > n1:
         answer += 4
+
+    elif n5 > n2 and n5 > n3 and n5 > n4 and n5 > n1:
+        answer += 5
 
     return answer
 
