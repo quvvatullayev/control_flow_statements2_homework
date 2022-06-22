1bd445945cf3211101a25e2534ea0af678cba524
tajriba <quvvatullayev@gmail.com>
2022-06-22 12:21:59 +0300

    if07.py

--- if07.py
+++ if07.py
@@ -18,16 +18,16 @@ def main(temp):
     if temp < 0:
         answer += "Freezing"
 
-    elif 1 <= temp and temp <= 10:
+    elif 1 <= temp <= 10:
         answer += "Freezing"
 
-    elif 11 <= temp and temp <= 20:
+    elif 11 <= temp <= 20:
         answer += "Cold"
     
-    elif 21 <= temp and temp <= 30:
+    elif 21 <= temp <= 30:
         answer += "Normal"
 
-    elif 31 <= temp and temp <= 40:
+    elif 31 <= temp <= 40:
         answer += "Hot"
     
     elif temp > 40:
