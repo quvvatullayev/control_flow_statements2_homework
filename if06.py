25aed43ddc57faa0cb707587c673cc20f14d1a40
tajriba <quvvatullayev@gmail.com>
2022-06-22 08:32:24 +0300

    if06.py

--- if06.py
+++ if06.py
@@ -1,4 +1,6 @@
 def main(n):
+    answer = 0
+
     """
     Find the index of the largest digit of the five-digit number.
     Args:
@@ -6,4 +8,37 @@ def main(n):
     Returns:
         int: return answer.
     """
-    return
+
+    n1 = n % 10
+    n //= 10
+
+    n2 = n % 10
+    n //= 10
+
+    n3 = n % 10
+    n //= 10
+
+    n4 = n % 10
+    n //= 10
+
+    n5 = n % 10
+    n //= 10
+
+    if n1 > n2 and n1 > n3 and n1 > n4 and n1 > n5:
+        answer += 0
+
+    elif n2 > n1 and n2 > n3 and n2 > n4 and n2 > n5:
+        answer += 1
+
+    elif n3 > n1 and n3 > n2 and n3 > n4 and n3 > n5:
+        answer += 2
+
+    elif n4 > n2 and n4 > n3 and n4 > n5 and n4 > n1:
+        answer += 3
+
+    elif n5 > n2 and n5 > n3 and n5 > n4 and n5 > n1:
+        answer += 4
+
+    return answer
+
+print(main(45963))
