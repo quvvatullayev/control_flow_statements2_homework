bb3a477fcbe1b474fbbb2ed66d452ac67e20b0db
tajriba <quvvatullayev@gmail.com>
2022-06-22 06:26:35 +0300

    if01.py

--- if01.py
+++ if01.py
@@ -12,10 +12,30 @@ def main(a,b,c):
     if (a > b) or (a > c):
         max = a
 
+    if ((a > b) or (c > a)) and (c > b):
+        max = c
+    
+    if ((b > a) or (a > c)) and (b > c):
+        max = b
+
     if (b > a) or (b > c):
         max = b
+
+    if ((b > a) or (c > b)) and (c > a):
+        max = c
+    
+    if ((a > b) or (b > c)) and (a > c):
+        max = a
     
     if (c > a) or (c > b):
         max = c
-        
+
+    if ((c > a) or (b > c)) and (b > a):
+        max = b
+
+    if ((a > c) or (c > b)) and (a > b):
+        max = a
+
     return max
+
+print(main(8,-61,7))
